35e6f6deabf2a571e0e6fbf4dd06609530dd32a1
TonPaes <36853529+TonPaes@users.noreply.github.com>
2019-11-12 21:36:25 -0300

    Add files via upload

---
+++ Learning.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Learning(){
+    return (
+        <ul>
+            <li>uno</li>
+            <li>dos</li>
+            <li>tres</li>
+            <li>QATORZE!!!</li>
+        </ul>
+    );
+
+}
+
+export default Learning;
